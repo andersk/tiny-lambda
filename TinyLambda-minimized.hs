c37b91ee3d5a701d373598917e737c1c8f7ed923
Anders Kaseorg <andersk@mit.edu>
2016-03-08 05:43:01 -0500

    −2 characters
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- TinyLambda-minimized.hs
+++ TinyLambda-minimized.hs
@@ -2,5 +2,5 @@ data T=T{a::T->T,(%)::ShowS}
 i d=T(i. \x v->'(':d v++' ':x%v++")")d
 l f v="(λ "++v++". "++f(i(\_->v))%('x':v)++")"
 (?)=q.lex
-q[(v,s)]k|v/="("=k(foldl1 a.lookup v)s|'λ':u<-s,[(w,_:t)]<-lex u=t? \b->k(\e->T<*>l$b.(:e).(,)w).tail|0<1=s? \f->(? \x->k(a.f<*>x).tail)
+q[(v,s)]k|v/="("=k(foldl1 a.lookup v)s|'λ':u<-s,[(w,_:t)]<-lex u=t? \b->k(\e->T<*>l$b.(:e).(,)w).tail|0<1=s? \f->(?(.tail).k.(a.f<*>))
 main=interact(? \f->(f[]%"x"++))
